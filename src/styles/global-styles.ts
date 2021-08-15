bf52aba7506739566976c30142d78ff771e46732
rounak <rounakjainofficial@gmail.com>
2021-08-15 21:49:48 +0530

    include Nunito font and use it as default globally

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -2,21 +2,24 @@ import { createGlobalStyle } from 'styled-components';
 import { StyleConstants } from './StyleConstants';
 /* istanbul ignore next */
 export const GlobalStyle = createGlobalStyle`
+
   html,
   body {
     height: 100%;
     width: 100%;
     line-height: 1.5;
+    box-sizing: border-box;
   }
 
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    padding-top: ${StyleConstants.NAV_BAR_HEIGHT};
+    // padding-top: ${StyleConstants.NAV_BAR_HEIGHT};
     background-color: ${p => p.theme.background};
   }
 
   body.fontLoaded {
-    font-family: 'Inter', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-family:  'Nunito', sans-serif;
+    letter-spacing: 1.1px;
   }
   
   p,
