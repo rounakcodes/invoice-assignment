6cd513d6842b972f6e00ea946501596324ee9499
rounak <rounakjainofficial@gmail.com>
2021-08-16 06:48:13 +0530

    correct meta tag for searcth engine

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -52,10 +52,7 @@ export function HomePage() {
     <>
       <Helmet>
         <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React Boilerplate application homepage"
-        />
+        <meta name="description" content="An invoicing application" />
       </Helmet>
       <Heading style={HeadingStyle}>
         Dashboard
